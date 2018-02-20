ccccefcf13cd8a21f1e031bc6ba4e38addd31846
bhekidube <bhekinkosidube@gmail.com>
2018-02-20 13:59:43 +0200

    New MVC project
    New MVC project

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Services;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IMailService _mail;
+
+        public HomeController(IMailService mail)
+        {
+            _mail = mail;
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(ContactModel model)
+        {
+            var msg = string.Format("Comment from: {1}{0}Email:{2}{0}Comment",
+                Environment.NewLine,
+                model.Name,
+                model.Email,
+                model.Comment         
+                );
+
+            if (_mail.SendMail("noreply@topcaves.com", "bhekinkosidube@gmail.com", "Contact us Email", msg))
+            {
+                ViewBag.MailSent = true;
+            }
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult MyMessages()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Models/ContactModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Controllers
+{
+    public class ContactModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Comment { get; set; }
+
+    }
+}
